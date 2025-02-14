25b97608786902b91ad99cbf53e217eda8f8b435
tejagorakati <gorakatiteja07@gmail.com>
2025-02-15 01:36:38 +0530

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,9 @@
+function goToRound(roundNumber) {
+    if (roundNumber === 1) {
+        window.location.href = "round1.html";
+    } else if (roundNumber === 2) {
+        window.location.href = "round2.html";
+    } else if (roundNumber === 3) {
+        window.location.href = "round3.html";
+    }
+}
